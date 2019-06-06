c54ca212896c126bbd1f8d41001e78a8e5652992
shibathaslin <51432456+shibathaslin@users.noreply.github.com>
2019-06-06 12:07:00 +0530

    Create vc.py

---
+++ vc.py
@@ -0,0 +1,6 @@
+let=input()
+if(let=='a' or let=='e' or let=='i' or let=='o' or let=='u' or let=='A' or let=='E' or let=='I' or 
+   let=='O' or let=='U'):
+    print("Vowel")
+else:
+    print("Consonant")
