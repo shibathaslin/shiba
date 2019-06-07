bafa03431639f2df1caae40ca46226ee80133936
shibathaslin <51432456+shibathaslin@users.noreply.github.com>
2019-06-07 14:44:52 +0530

    Create swa.py

---
+++ swa.py
@@ -0,0 +1,16 @@
+#s
+a1=input()
+a2=input()
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+l1=[]
+for i,j in zip(a1,a2):
+	l1.append((l.index(i)+1)+(l.index(j)+1))
+#print(l1)
+for i in range(0,len(l1)):
+	if l1[i]>26:
+		l1[i]=l1[i]%26
+#print(l1)		
+for i in l1:
+    
+	print(l[i-1],end="")
+© 2019 GitHub, Inc.
